58574ce5198e0bacec0c3552f4b8018a90f4ae1d
oveadee <a.szczechura.98@gmail.com>
2023-01-04 16:26:45 +0100

    chore: cleanup

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { ColorModeScript } from "@chakra-ui/react";
+import { Html, Head, Main, NextScript } from "next/document";
+import theme from "../theme";
+
+const Document = () => {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
