10ad18822c050301f1ada962bec5f886182bd7c8
szczechurek <r.szczechura.01@gmail.com>
2023-01-28 13:49:01 +0100

    feat: add signin onSubmit handler

--- src/views/signin/schema.ts
+++ src/views/signin/schema.ts
@@ -1,6 +1,6 @@
 import * as yup from "yup";
 
-export const loginSchema = yup.object().shape({
+export const signinSchema = yup.object().shape({
   email: yup.string().email().required(),
   password: yup.string().min(8).max(32).required(),
 });
